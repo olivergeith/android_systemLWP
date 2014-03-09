a980a077b9ec0a835fe4aeb3202a42ea60e2f6ed
olivergeith <oliver.geith@gmx.net>
2014-03-09 21:11:25 +0100

    ZooperWide V3 implementiert

--- src/de/geithonline/systemlwp/BitmapDrawerBigArc.java
+++ src/de/geithonline/systemlwp/BitmapDrawerBigArc.java
@@ -14,7 +14,11 @@ public class BitmapDrawerBigArc {
 	private final int		cHeight;
 	private final int		cWidth;
 	private final Canvas	canvas;
-	private final int		d	= 30;
+	private final int		offset		= 5;
+	private final int		bogenDicke	= 30;
+	private final int		skaleDicke	= 100;
+	private final int		abstand		= 8;
+	private final float		gap			= 2f;
 
 	public BitmapDrawerBigArc(final Canvas canvas) {
 		this.canvas = canvas;
@@ -22,24 +26,105 @@ public class BitmapDrawerBigArc {
 		cHeight = canvas.getHeight();
 	}
 
-	public Bitmap drawBitmap(final int level) {
+	public void draw(final int level) {
+		canvas.drawBitmap(drawBogen(level), 0, cHeight - cWidth / 2 - 5, null);
+		canvas.drawBitmap(drawSegmente(level), 0, cHeight - cWidth / 2 - 5, null);
+		canvas.drawBitmap(drawZeiger(level), 0, cHeight - cWidth / 2 - 5, null);
+	}
 
+	private Bitmap drawBogen(final int level) {
+		final Bitmap bitmap = Bitmap.createBitmap(cWidth, cWidth / 2, Bitmap.Config.ARGB_8888);
+		final Canvas bitmapCanvas = new Canvas(bitmap);
+
+		// Background
+		bitmapCanvas.drawArc(getRectForOffset(offset), 180, 180, true, getBackgroundPaint());
+		// Level
+		bitmapCanvas.drawArc(getRectForOffset(offset), 180, Math.round(level * 1.8), true, getBatteryPaint(level));
+		// delete inner Circle
+		bitmapCanvas.drawArc(getRectForOffset(offset + bogenDicke), 0, 360, true, getErasurePaint());
+
+		return bitmap;
+	}
+
+	private Bitmap drawSegmente(final int level) {
+		final Bitmap bitmap = Bitmap.createBitmap(cWidth, cWidth / 2, Bitmap.Config.ARGB_8888);
+		final Canvas bitmapCanvas = new Canvas(bitmap);
+
+		final int segmente = 10;
+		final float winkelOneSegment = (180f - (segmente - 1) * gap) / segmente;
+		final int zehner = level / 10;
+
+		final int off = offset + bogenDicke + abstand;
+
+		// Skala Hintergergrund einer
+		Paint paint;
+		for (int i = 0; i < segmente; i++) {
+			if (i < zehner || level == 100) {
+				paint = getBatteryPaint(level);
+			} else {
+				paint = getBackgroundPaint();
+			}
+			final float startwinkel = 180f + i * (winkelOneSegment + gap);
+			bitmapCanvas.drawArc(getRectForOffset(off), startwinkel, winkelOneSegment, true, paint);
+		}
+
+		// delete inner Circle
+		bitmapCanvas.drawArc(getRectForOffset(off + skaleDicke), 0, 360, true, getErasurePaint());
+		return bitmap;
+	}
+
+	private Bitmap drawZeiger(final int level) {
+		final Bitmap bitmap = Bitmap.createBitmap(cWidth, cWidth / 2, Bitmap.Config.ARGB_8888);
+		final Canvas bitmapCanvas = new Canvas(bitmap);
+
+		final int segmente = 10;
+		final float winkelOneSegment = (180f - (segmente - 1) * gap) / segmente;
+		int zehner = level / 10;
+		final int einer = level % 10;
+
+		final int off = offset + bogenDicke + abstand;
+		final int radiusDelta = skaleDicke * einer / 10;
+
+		// Skala Hintergergrund einer
+		final Paint paint = getBatteryPaint(level);
+		if (zehner == 10)
+			zehner = 9;
+		final float startwinkel = 180f + zehner * (winkelOneSegment + gap);
+
+		bitmapCanvas.drawArc(getRectForOffset(off + skaleDicke - radiusDelta), startwinkel, winkelOneSegment, true, paint);
+
+		// delete inner Circle
+		bitmapCanvas.drawArc(getRectForOffset(off + skaleDicke), 0, 360, true, getErasurePaint());
+		return bitmap;
+	}
+
+	private Paint getBatteryPaint(final int level) {
 		final Paint paint = new Paint();
 		paint.setAntiAlias(true);
 		paint.setColor(Color.WHITE);
 		paint.setStyle(Style.FILL);
+		return paint;
+	}
 
-		final Bitmap bitmap = Bitmap.createBitmap(cWidth, cWidth / 2, Bitmap.Config.ARGB_8888);
-		final Canvas c = new Canvas(bitmap);
-		c.drawArc(new RectF(5, 5, cWidth - 5, cWidth - 5), 180, Math.round(level * 1.8), true, paint);
+	private Paint getBackgroundPaint() {
+		final Paint paint = new Paint();
+		paint.setAntiAlias(true);
+		paint.setColor(Color.DKGRAY);
+		paint.setStyle(Style.FILL);
+		return paint;
+	}
 
-		// delete inner Circle
+	private Paint getErasurePaint() {
+		final Paint paint = new Paint();
 		paint.setColor(Color.TRANSPARENT);
 		final PorterDuffXfermode xfermode = new PorterDuffXfermode(Mode.CLEAR);
 		paint.setXfermode(xfermode);
 		paint.setStyle(Style.FILL);
-		c.drawArc(new RectF(d, d, cWidth - d, cWidth - d), 0, 360, true, paint);
-		return bitmap;
+		return paint;
+	}
+
+	private RectF getRectForOffset(final int offset) {
+		return new RectF(offset, offset, cWidth - offset, cWidth - offset);
 	}
 
 }
--- src/de/geithonline/systemlwp/LiveWallpaperService.java
+++ src/de/geithonline/systemlwp/LiveWallpaperService.java
@@ -4,15 +4,13 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.Paint.Style;
 import android.graphics.RectF;
 import android.os.Handler;
 import android.service.wallpaper.WallpaperService;
 import android.view.SurfaceHolder;
 
 public class LiveWallpaperService extends WallpaperService {
-	int	x, y, level;
+	int	level;
 
 	@Override
 	public void onCreate() {
@@ -42,11 +40,7 @@ public class LiveWallpaperService extends WallpaperService {
 		public Bitmap			image1, backgroundImage;
 
 		MyWallpaperEngine() {
-			// get the fish and background image references
-			image1 = BitmapFactory.decodeResource(getResources(), R.drawable.fish);
 			backgroundImage = BitmapFactory.decodeResource(getResources(), R.drawable.background);
-			x = 0; // initialize x position
-			y = 200; // initialize y position
 			level = 0;
 
 		}
@@ -83,50 +77,29 @@ public class LiveWallpaperService extends WallpaperService {
 		void draw() {
 			final SurfaceHolder holder = getSurfaceHolder();
 
-			Canvas c = null;
+			Canvas canvas = null;
 			try {
-				c = holder.lockCanvas();
+				canvas = holder.lockCanvas();
 				// get the size of canvas
-				final int width = c.getWidth();
-				final int height = c.getHeight();
-
-				// clear the canvas
-				c.drawColor(Color.BLACK);
-				if (c != null) {
+				final int width = canvas.getWidth();
+				final int height = canvas.getHeight();
+				if (canvas != null) {
+					// clear the canvas
+					canvas.drawColor(Color.BLACK);
 					// draw the background image and stretch it to canvas
-					// c.drawBitmap(backgroundImage, 0, 0, null);
-					c.drawBitmap(backgroundImage, null, new RectF(0, 0, width, height), null);
-					// draw the fish
-					c.drawBitmap(image1, x, y, null);
+					canvas.drawBitmap(backgroundImage, null, new RectF(0, 0, width, height), null);
 
-					final BitmapDrawerCircle drawerCircle = new BitmapDrawerCircle(c);
-					c.drawBitmap(drawerCircle.drawBitmap(level), width / 2 - 32, height / 2 - 32, null);
+					final BitmapDrawerBigArc bigArc = new BitmapDrawerBigArc(canvas);
+					bigArc.draw(level);
 
-					final BitmapDrawerBigArc bigArc = new BitmapDrawerBigArc(c);
-					c.drawBitmap(bigArc.drawBitmap(level), 0, height - width / 2 - 5, null);
-
-					// draw text
-					final Paint paint = new Paint();
-					paint.setColor(Color.WHITE);
-					paint.setStyle(Style.FILL);
-					c.drawText("Hello World", x, 300, paint);
-
-					// if x crosses the width means x has reached to right edge
-					if (x > width) {
-						// assign initial value to start with
-						x = 0;
-					}
-					// change the x position/value by 1 pixel
-					x = x + 1;
 					level += 1;
-
 					if (level > 100)
 						level = 0;
 
 				}
 			} finally {
-				if (c != null)
-					holder.unlockCanvasAndPost(c);
+				if (canvas != null)
+					holder.unlockCanvasAndPost(canvas);
 			}
 
 			handler.removeCallbacks(drawRunner);
