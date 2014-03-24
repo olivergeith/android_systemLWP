21ee25f36581f760ca9538cfaeaea0cf0fa15660
olivergeith <oliver.geith@gmx.net>
2014-03-24 15:12:55 +0100

    Adding Button to set Wallpaper if not running

--- src/de/geithonline/systemlwp/PreferencesActivity.java
+++ src/de/geithonline/systemlwp/PreferencesActivity.java
@@ -2,10 +2,23 @@ package de.geithonline.systemlwp;
 
 import java.util.List;
 
+import android.annotation.SuppressLint;
+import android.app.ActivityManager;
+import android.app.ActivityManager.RunningServiceInfo;
+import android.app.WallpaperManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup.LayoutParams;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class PreferencesActivity extends PreferenceActivity {
 
@@ -28,12 +41,34 @@ public class PreferencesActivity extends PreferenceActivity {
 
 		billingManager = new BillingManager(this);
 		final boolean isPremium = billingManager.isPremium();
+		// A button to set us as Wallpaper
+		final Button b = new Button(this);
+		b.setText("Set Wallpaper");
+		b.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(final View v) {
+				setWallpaper();
+			}
+		});
+		// A View because there might be another button (billing)
+		final LinearLayout ll = new LinearLayout(this);
+		ll.setOrientation(LinearLayout.VERTICAL);
+		ll.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
+		ll.setGravity(Gravity.CENTER);
+		if (!isMyServiceRunning(LiveWallpaperService.class.getName())) {
+			ll.addView(b);
+		} else {
+			// do nothing ll.addView(new Button(this));
+		}
 
 		// Add a button to the header list.
 		if (!isPremium) {
 			final Button button = billingManager.getButton();
-			setListFooter(button);
+			ll.addView(button);
 		}
+		// set view with buttons to the list footer
+		setListFooter(ll);
 	}
 
 	/**
@@ -48,5 +83,32 @@ public class PreferencesActivity extends PreferenceActivity {
 	protected void onDestroy() {
 		super.onDestroy();
 		billingManager.onDestroy();
+	}
+
+	@SuppressLint("InlinedApi")
+	private void setWallpaper() {
+		final Intent i = new Intent();
+
+		if (Build.VERSION.SDK_INT >= 16) {
+			i.setAction(WallpaperManager.ACTION_CHANGE_LIVE_WALLPAPER);
+
+			final String p = LiveWallpaperService.class.getPackage().getName();
+			final String c = LiveWallpaperService.class.getCanonicalName();
+			i.putExtra(WallpaperManager.EXTRA_LIVE_WALLPAPER_COMPONENT, new ComponentName(p, c));
+		} else {
+			i.setAction(WallpaperManager.ACTION_LIVE_WALLPAPER_CHOOSER);
+		}
+		startActivityForResult(i, 0);
+	}
+
+	private boolean isMyServiceRunning(final String className) {
+		final ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+		for (final RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+			Log.i("GEITH", "Sevice = " + service.service.getClassName());
+			if (className.equals(service.service.getClassName())) {
+				return true;
+			}
+		}
+		return false;
 	}
 }
